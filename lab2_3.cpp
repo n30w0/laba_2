b748638902841c58805b8813800e2eb75f10bf38
n30w0 <78935701+n30w0@users.noreply.github.com>
2021-03-02 00:02:03 +0300

    Add files via upload

---
+++ lab2_3.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <conio.h>
+using namespace std;
+double fun(double x)
+{
+	return x <= 3 ? ((pow(x, 2)) - 3 * x + 9) : (1 / ((pow(x, 3)) + 6));
+}
+bool Proverka(double x, double y) {
+	if (abs(y) <= 1 && abs(y) >= abs(x))
+		return true;
+	else
+		return false;
+}
+int main() {
+	double x, y;
+	cout << "Viberite zadanie (1,2): " << endl;
+	cout << "Zadanie 1 " << endl;
+	cout << "Zadanie 2 " << endl;
+	switch (_getch()) {
+	case '1':
+		cout << "x="; cin >> x;
+		cout << "y="; cin >> y;
+		cout << "Result: " << Proverka(x, y) << endl;
+		break;
+	case '2':
+		system("cls");
+		cout << "x = "; cin >> x;
+		cout << "f = " << fun(x) << endl;
+		break;
+	default:
+		cout << "Nepravil'niy vibor" << endl;
+		break;
+	}
+}
+	
---
+++ lab_2.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+bool Proverka(double x, double y) {
+	if (fabs(y) <= 1 && fabs(y) >= fabs(x))
+			return true;
+		else
+			return false;
+}
+int main() {
+	double x, y;
+	cout << "x="; cin >> x;
+	cout << "y="; cin >> y;
+	cout << "Result: " << Proverka(x, y) << endl;
+}
